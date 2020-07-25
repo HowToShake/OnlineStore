0410a20540c16d9649c8fa61c479a7f9a520071b
HowToShake <ryskiewicz.m@gmail.com>
2020-07-25 19:48:29 +0200

    Initial version of navbar

--- shop/src/components/TopBar/top-bar-view.js
+++ shop/src/components/TopBar/top-bar-view.js
@@ -1,12 +1,32 @@
 import React from 'react'
-
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
 export const TopBar = (props) => {
 
     console.log(props);
 
     return (
-        <>
-        </>
+        <Navbar  expand="lg" variant="light" style={{backgroundColor: '#e0e0e0'}}>
+        <Navbar.Brand href="#home">Online Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="secondary">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+        </Navbar>
     )
 
 }
