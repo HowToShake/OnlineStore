5ffc8147143f44a570f607b2f78251a576ee9554
HowToShake <ryskiewicz.m@gmail.com>
2020-08-02 15:34:08 +0200

    Register layout done. Before implementing logic

--- shop/src/components/register/register-view.js
+++ shop/src/components/register/register-view.js
@@ -1,29 +1,62 @@
 import React from 'react'
 import { Form, Input, Button } from 'antd';
 
-
 const Register = () => {
 
     const onFinish = values => {
         console.log('Success:', values);
-      };
+    };
     
-      const onFinishFailed = errorInfo => {
+    const onFinishFailed = errorInfo => {
         console.log('Failed:', errorInfo);
+    };
+
+    const confirmPasswords = ({getFieldValue}) => ({
+        validator (rule, value) {
+        if (!value || getFieldValue("password") === value) {
+            return Promise.resolve();
+        }
+
+        return Promise.reject(
+            "The two passwords that you entered do not match!"
+        );
+        }
+    })
+
+    const formItemLayout = {
+        labelCol: {
+          xs: {
+            span: 24
+          },
+          sm: {
+            span: 8
+          }
+        },
+        wrapperCol: {
+          xs: {
+            span: 24
+          },
+          sm: {
+            span: 16
+          }
+        }
       };
 
+
     return (
-        <div className="d-flex flex-column justify-content-center" style={{border: '1px dashed red'}}>
-            <div className="w-25 align-self-center pt-5">
+        <div className="d-flex flex-column justify-content-center" style={{border: '1px dashed red', minWidth: '100vw'}}>
+            <div className="align-self-center pt-5 align-self-center" style={{width: '40vw'}}>
                 <Form
-                    name="basic"
+                    {...formItemLayout}
+                    name="register"
                     initialValues={{
-                        remember: true,
+                        remember: false,
                     }}
                     onFinish={onFinish}
                     onFinishFailed={onFinishFailed}
                     >
                     <Form.Item
+    
                         label="Username"
                         name="username"
                         hasFeedback
@@ -32,6 +65,25 @@ const Register = () => {
                             required: true,
                             message: 'Please input your username!',
                         },
+                        ]}
+                    >
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item
+                
+                        name="email"
+                        label="E-mail"
+                        hasFeedback
+                        rules={[
+                        {
+                            type: "email",
+                            message: "The input is not valid E-mail!"
+                        },
+                        {
+                            required: true,
+                            message: "Please input your E-mail!"
+                        }
                         ]}
                     >
                         <Input />
@@ -51,9 +103,25 @@ const Register = () => {
                         <Input.Password />
                     </Form.Item>
 
+                    <Form.Item
+                        name="confirm"
+                        label="Confirm Password"
+                        dependencies={["password"]}
+                        hasFeedback
+                        rules={[
+                        {
+                            required: true,
+                            message: "Please confirm your password!"
+                        },
+                        ({ getFieldValue }) => confirmPasswords({ getFieldValue })
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-                    <Form.Item  style={{border: '1px solid green'}}>
-                        <Button className="ml-auto" type="primary" htmlType="submit">
+                
+                    <Form.Item  className="pt-4" style={{marginLeft: '50%', marginRight: '50%'}}>
+                        <Button type="primary" htmlType="submit">
                             Submit
                         </Button>
                     </Form.Item>
