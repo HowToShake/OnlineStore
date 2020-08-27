5c69e6c6f9d8212ad08491b498756ac0ca138ce6
HowToShake <ryskiewicz.m@gmail.com>
2020-08-27 20:35:38 +0200

    Landing page style changed

--- shop/client/src/components/LandingPage/landing-page-view.js
+++ shop/client/src/components/LandingPage/landing-page-view.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import LandingPageStyles from "./landing-page-view.module.scss";
 import { LoadingOutlined, ShoppingCartOutlined } from "@ant-design/icons";
-import { Card, Button, message } from "antd";
+import { Card, Button, message, Menu } from "antd";
 import { getWindowDimensions } from "../../models/common-method";
+import SubMenu from "antd/lib/menu/SubMenu";
 
 const { Meta } = Card;
 
@@ -25,6 +26,7 @@ export const LandingPage = ({ props, mapDispatchToProps }) => {
     handleResize();
     return () => {
       window.removeEventListener("resize", handleResize);
+      document.querySelector('div#root').style.backgroundColor = '';
     };
   }, [props.user]);
 
@@ -87,5 +89,37 @@ export const LandingPage = ({ props, mapDispatchToProps }) => {
     }
   };
 
-  return <>{loadItems()}</>;
+  const switchCategory = (category) => {
+    console.log(category);
+    switch(category){
+      case 'Pop': {
+        document.querySelector('div#root').style.backgroundColor ='#F4D7D7'
+        break;
+      }
+      case 'Rock' :{
+        document.querySelector('div#root').style.backgroundColor = 'pink'
+        break;
+      }
+    }
+  }
+
+  const categories = ['Pop', 'Rock', 'Hip Hop', 'Electronic', 'Indie', 'Soul', 'Jazz']
+
+  return(
+    <>
+      <Menu onClick={() => console.log('hejka')} mode='horizontal' style={{width: '100vw'}}>
+        {categories.map((category, index) => {
+          return(
+            <Menu.Item key={index} style={{width: `${Math.floor(99/(categories.length + 1))}vw`}} onClick={() => switchCategory(category)}>
+              <p style={{padding: 0, margin: 0, textAlign: 'center'}}>{category}</p>
+            </Menu.Item>
+          )
+        })}
+      </Menu>
+
+      <div className="LandingPage">
+
+      </div>
+    </>
+  )
 };
