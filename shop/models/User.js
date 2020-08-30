f559e75a2b88913d8098f6f8c73c59e99c801123
HowToShake <ryskiewicz.m@gmail.com>
2020-08-30 14:05:33 +0200

    User model updated. Now keep all orders, and every user has an private role

--- shop/models/User.js
+++ shop/models/User.js
@@ -2,18 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-    },
-    password:{
-        type: String,
-        required: true,
-    },
+    name:{type: String, required: true },
+    email:{type: String, required: true },
+    password:{type: String,required: true },
+    role: {type: String, required: true },
+    orders:[
+        {
+            status: {type: String, required: true },
+            orderedItems: [{
+                itemID: {type: String, required: true },
+                amount: {type: Number, required: true }
+            }],
+            price: {type: String, required: true },
+            receiverInfo: {
+                name: {type: String, required: true },
+                surname: {type: String, required: true },
+                address: {type: String, required: true },
+                phoneNumber: {type: String, required: true },
+                deliveryMethod: {type: String, required: true }
+            }
+        }
+    ]
 });
 
 const User  = mongoose.model('user', UserSchema);
--- shop/routes/api/users.js
+++ shop/routes/api/users.js
@@ -1,66 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
+router.post("/", (req, res) => {
+  const { name, email, password, role = "user", orders = [] } = req.body;
 
+  if (!name || !email || !password) {
+    res.status(400).json({ msg: "Enter all fields!" });
+  }
 
-router.post('/',(req, res) => {
-    const {name,email,password} = req.body;
-
-    if(!name || !email || !password ){
-        res.status(400).json({msg: "Enter all fields!"});
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ msg: "User already exists!" });
     }
+    const newUser = new User({
+      name,
+      email,
+      password,
+      role,
+      orders,
+    });
 
-    User.findOne({email})
-    .then(user => {
-        if(user){
-            return res.status(400).json({msg: "User already exists!"});
-        }
-        const newUser = new User({
-            name,
-            email,
-            password,
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then((user) => {
+          jwt.sign(
+            { id: user.id },
+            config.get("jwtSecret"),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                  role: user.role,
+                  orders: user.orders,
+                },
+              });
+            }
+          );
         });
-
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) =>{
-                if(err) throw err;
-                newUser.password = hash;
-                newUser.save()
-                    .then(user => {
-
-                        jwt.sign(
-                            { id: user.id },
-                            config.get('jwtSecret'),
-                            { expiresIn: 3600 },
-                            (err, token) => {
-                                if(err) throw err;
-
-                                res.json({
-                                    token,
-                                    user:{
-                                        id: user.id,
-                                        name: user.name,
-                                        email: user.email,
-                                    }
-                                })
-
-                            }
-                        )
-
-                        
-                    })
-            })
-        })
-
-    })
-
-   // res.send('register');
+      });
+    });
+  });
 });
 
+router.put("/:id", (req, res) => {
+  User.updateOne({
+    $push: {
+      orders: [
+        {
+          status: "Pending",
+          orderedItems: [
+            {
+              itemID: "5f2e859c4c3aaa23e0de220a",
+              amount: "3",
+            },
+            {
+              itemID: "5f2e882745fded2e6835a68e",
+              amount: "1",
+            },
+          ],
+          price: "$5",
+          receiverInfo: {
+            name: "JA",
+            surname: "MY",
+            address: "ULICA 16 MIASTO, KOD-POCZTOWY",
+            phoneNumber: "+48 123 456 789",
+            deliveryMethod: "PACZKOMAT",
+          },
+        },
+      ],
+    },
+  })
+    .then((items) => res.json({ success: true }))
+    .catch((err) => res.status(404).json({ success: false }));
+});
 
-module.exports =  router;
+module.exports = router;
