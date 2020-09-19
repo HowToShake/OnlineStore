dc9aa794211f7817e9b3fb262f6824e0a129b149
unknown <ryskiewicz.m@gmail.com>
2020-09-19 23:20:07 +0200

    Before displaying categories

--- shop/client/src/components/Category/category-view.js
+++ shop/client/src/components/Category/category-view.js
@@ -1,23 +1,18 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react"
 
-export const CategoryView = ({props, mapDispatchToProps}) => {
+export const CategoryView = ({ props, mapDispatchToProps }) => {
     useEffect(() => {
-        const category = window.location.href.substring((window.location.href.indexOf('?')+1));
-        mapDispatchToProps.getItemsInParticularCategory(category);
-
+        const category = window.location.href.substring(window.location.href.indexOf("?") + 1)
+        mapDispatchToProps.getItemsInParticularCategory(category)
     }, [])
 
-    const uniqueBands = [...new Set(props.musicInParticularCategory.map(item => item.band))];
- 
-    const renderSectionForPartiucalrBand = () => {
-        uniqueBands.map(el => {
-           return (<p>xD</p>)
-        })
-    }
+    const uniqueBands = [...new Set(props.musicInParticularCategory.map((item) => item.band))]
 
-    return(
+    return (
         <>
-            {renderSectionForPartiucalrBand()}
+            {uniqueBands.map((el, index) => {
+                return <p>{el}</p>
+            })}
         </>
     )
 }
