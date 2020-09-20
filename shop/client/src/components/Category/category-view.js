e2e834723ab41b13dd6645ce4ef3b15a7c9368ae
unknown <ryskiewicz.m@gmail.com>
2020-09-20 11:39:15 +0200

    Category effect achived

--- shop/client/src/components/Category/category-view.js
+++ shop/client/src/components/Category/category-view.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react"
-import style from './category-view.module.scss'
+import { Card } from "antd"
+import style from "./category-view.module.scss"
+import Meta from "antd/lib/card/Meta"
 
 export const CategoryView = ({ props, mapDispatchToProps }) => {
     useEffect(() => {
@@ -8,32 +10,38 @@ export const CategoryView = ({ props, mapDispatchToProps }) => {
     }, [])
 
     const uniqueBands = [...new Set(props.musicInParticularCategory.map((item) => item.band))]
+    const multipleUniqueBands = uniqueBands.concat(uniqueBands).sort()
 
     const renderUniqueBands = () => {
-        return uniqueBands.map((uniqueBand, index) => {
-            let firstTime = true
-            return props.musicInParticularCategory.map((el, index) => {
-                if (uniqueBand === el.band) {
-                    if (firstTime) {
-                        firstTime = false
-                        return (
-                            <>
-                                <h1 key={el.band}>{el.band}</h1>
-                                <p key={el.albumName}>{el.albumName}</p>
-                            </>
-                        )
-                    } else {
-                        return (
-                            <>
-                                <p key={el.albumName}>{el.albumName}</p>
-                            </>
-                        )
-                    }
-                }
-            })
+        return multipleUniqueBands.map((uniqueBand, index) => {
+            if (multipleUniqueBands[index] === multipleUniqueBands[index + 1])
+                return (
+                    <>
+                        <h1 key={index}>{uniqueBand}</h1>
+                    </>
+                )
+            else {
+                return(
+                    <div className={style.cardContainer}>
+                        {props.musicInParticularCategory.map((el, index) => {
+                            if (el.band === uniqueBand)
+                                return (
+                                    <>
+                                        <Card className={style.Card}>
+                                            <Meta title={el.albumName} description="placeholder" />
+                                        </Card>
+                                    </>
+                                )
+                        })}
+                    </div>
+                )
+            }
         })
     }
 
-    return(
-         <div className={style.container}>{renderUniqueBands()}</div>)
+    return (
+        <>
+            <div className={style.container}>{renderUniqueBands()}</div>
+        </>
+    )
 }
