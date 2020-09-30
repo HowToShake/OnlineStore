c99e302f29913603adee0cefe1f7ec370fdcdd38
unknown <ryskiewicz.m@gmail.com>
2020-09-30 21:54:39 +0200

    Search page done.

--- shop/client/src/components/SearchPage/search-page-connected.js
+++ shop/client/src/components/SearchPage/search-page-connected.js
@@ -1,16 +1,19 @@
 import { connect } from "react-redux"
 import { SearchPage } from "./search-page-view"
 import { uploadItems } from "../../redux/actions/items"
+import { addItemToCart } from "../../redux/actions/cart"
 
 const mapStateToProps = (state) => ({
     props: {
         searchedItems: state.items.searchedItems,
+        user: state.auth.user,
     },
 })
 
 const mapDispatchToProps = (dispatch) => ({
     mapDispatchToProps: {
         uploadItems: () => dispatch(uploadItems()),
+        onAddItemToCartWasPressed: (selectedItem) => dispatch(addItemToCart(selectedItem)),
     },
 })
 
--- shop/client/src/components/SearchPage/search-page-view.js
+++ shop/client/src/components/SearchPage/search-page-view.js
@@ -1,20 +1,73 @@
 import React from "react"
+import Meta from "antd/lib/card/Meta"
+import { Card, Button, message } from "antd"
+import { ShoppingCartOutlined } from "@ant-design/icons"
+import style from './search-page-view.module.scss'
 
 export const SearchPage = ({ props, mapDispatchToProps }) => {
+
+    const addItem = (element) => {
+        if (props.user) {
+            mapDispatchToProps.onAddItemToCartWasPressed(element)
+        } else {
+            message.error("Only authenticated user can add products to Cart!", 1.5)
+        }
+    }
+
+    const uniqueBands = [...new Set(props.searchedItems.map((item) => item.band))]
+    const multipleUniqueBands = uniqueBands.concat(uniqueBands).sort()
+
     const renderSearchedItems = () => {
-        return (
-            <>
-                {props.searchedItems.map((el, index) => {
-                    return <h1 key={index}>{el.albumName}</h1>
-                })}
-            </>
-        )
+        return multipleUniqueBands.map((uniqueBand, index) => {
+            if (multipleUniqueBands[index] === multipleUniqueBands[index + 1])
+                return (
+                    <>
+                        <h1 key={index}>{uniqueBand}</h1>
+                    </>
+                )
+            else {
+                return (
+                    <div className={style.cardContainer}>
+                        {props.searchedItems.map((el, index) => {
+                            if (el.band === uniqueBand) {
+                                const price = `Price: ${el.price}$ `
+                                let available = "available"
+                                if (el.amount === 0) {
+                                    available = "unavailable"
+                                }
+
+                                return (
+                                    <>
+                                        <Card
+                                            key={el.albumName + index}
+                                            cover={
+                                                <img
+                                                    alt="img"
+                                                    src={"xd"} //TODO add el.imgURL
+                                                />
+                                            }
+                                            actions={[
+                                                <Button onClick={() => addItem(el)}>
+                                                    <ShoppingCartOutlined />
+                                                </Button>,
+                                            ]}
+                                            className={style.Card}>
+                                            <Meta key={el.albumName + index + price} title={el.albumName} description={price +` | `+available +` | `+`Category: ${el.category}`} />
+                                        </Card>
+                                    </>
+                                )
+                            }
+                        })}
+                    </div>
+                )
+            }
+        })
     }
 
     return (
-        <div>
+        <>
             <h1>SEARCH</h1>
-            {renderSearchedItems()}
-        </div>
+            <div className={style.container}>{renderSearchedItems()}</div>
+        </>
     )
 }
