34cd37f8493da6c3aaeb3b324a0c85ce5387bc69
mr_calcode <ryskiewicz.m@gmail.com>
2020-10-06 23:13:59 +0200

    Table added to order page.

--- shop/client/src/components/Order/order-connected.js
+++ shop/client/src/components/Order/order-connected.js
@@ -1,19 +1,15 @@
 import { connect } from "react-redux"
 import { Order } from "./order-view"
 
-
 const mapStateToProps = (state) => ({
-    props: {
-        user: state.auth.user,
-        totalPrice: state.cart?.totalPrice,
-        orderedItems: state.cart?.orderedItems,
-    },
+    user: state.auth.user,
+    totalPrice: state.cart?.totalPrice,
+    orderedItems: state.cart?.orderedItems,
+    uniqueOrderItems: [...new Set(state.cart?.order.map((item) => item))],
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    mapDispatchToProps: {
-        
-    },
+    mapDispatchToProps: {},
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Order)
--- shop/client/src/components/Order/order-view.js
+++ shop/client/src/components/Order/order-view.js
@@ -1,22 +1,70 @@
 import React from "react"
 import axios from "axios"
+import {Table} from 'antd'
 
-export const Order = ({ props }) => {
+export const Order = ({ user, orderedItems, totalPrice, uniqueOrderItems }) => {
+    const columns = [
+        {
+            title: "Album Name",
+            dataIndex: "albumName",
+            key: "albumName",
+        },
+        {
+            title: "Band",
+            dataIndex: "band",
+            key: "band",
+        },
+        {
+            title: "Category",
+            dataIndex: "category",
+            key: "category",
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            key: "price",
+        },
+        {
+            title: "Amount",
+            dataIndex: "amount",
+            key: "amount",
+        },
+        {
+            title: "Total Price",
+            dataIndex: "totalPrice",
+            key: "totalPrice",
+        },
+    ]
+    
+    const dataSource = uniqueOrderItems.map((element) => {
+        const amount = orderedItems.find(obj => obj[element._id])
+        const totalPrice = orderedItems.find(obj => obj[element._id])
+        return{
+        key: element._id,
+        albumName: element.albumName,
+        band: element.band,
+        category: element.category,
+        price: element.price,
+        amount: amount[element._id],
+        totalPrice: totalPrice[element._id]*element.price,
+        }
+    })
+
 
     const submitOrder = async () => {
-        debugger;
         await axios
-            .put(`http://localhost:5000/api/users/${props.user?._id}`, {
-                orderedItems: props.orderedItems,
-                price: props.totalPrice,
+            .put(`http://localhost:5000/api/users/${user?._id}`, {
+                orderedItems: orderedItems,
+                price: totalPrice,
                 receiverInfo: "",
             })
             .then(() => console.log("success"))
             .catch((err) => console.log(err))
     }
+
     return (
         <div>
-            <h1>xD</h1>
+            <Table dataSource={dataSource} columns={columns} />
             <button onClick={submitOrder}>SUBMIT</button>
         </div>
     )
