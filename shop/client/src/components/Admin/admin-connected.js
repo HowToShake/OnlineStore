8a0db2f91d1f7ce4615666686c66172a0bad8873
Michał Ryśkiewicz <ryskiewicz.m@gmail.com>
2020-10-27 18:04:47 +0100

    Form added to admin page - before adding come back arrow in cart section

--- shop/client/src/components/Admin/admin-connected.js
+++ shop/client/src/components/Admin/admin-connected.js
@@ -1,12 +1,16 @@
 import { connect } from "react-redux"
 import { Admin } from "./admin-view"
+import { getDistinctCategories } from "../../redux/actions/items"
 
 const mapStateToProps = (state) => ({
     role: state.auth?.user?.role,
+    categories: state.items?.categories,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    mapDispatchToProps: {},
+    mapDispatchToProps: {
+        getDistinctCategories: () => dispatch(getDistinctCategories()),
+    },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Admin)
--- shop/client/src/components/Admin/admin-view.js
+++ shop/client/src/components/Admin/admin-view.js
@@ -1,9 +1,11 @@
 import React from "react"
 import NotFoundPage from "../404/404-view"
 import axios from "axios"
-import { message, Table, Button, Form, Input } from "antd"
-import { CheckOutlined, DeleteOutlined } from "@ant-design/icons"
+import { message, Table, Button, Form, Input, Select } from "antd"
+import { CheckOutlined } from "@ant-design/icons"
 import style from "./admin-view.module.scss"
+
+const { Option } = Select
 
 export class Admin extends React.PureComponent {
     constructor(props) {
@@ -165,8 +167,26 @@ export class Admin extends React.PureComponent {
         },
     ]
 
-    submitOrder = (values) => {
-        console.log(values)
+    formRef = React.createRef()
+
+    submitOrder = async (values) => {
+        await axios
+            .post("http://localhost:5000/api/items/", values)
+            .then((res) => {
+                if (res?.data.success) {
+                    this.showInfo(res?.data.success)
+                }
+                this.formRef.current.resetFields()
+            })
+            .catch((err) => console.log(err))
+    }
+
+    showInfo = (success) => {
+        if (success) {
+            message.success("You have put data to database! 😀 ")
+        } else {
+            message.error("There was some problems to send your data to database 🙁 Please try again later!")
+        }
     }
 
     render() {
@@ -177,17 +197,99 @@ export class Admin extends React.PureComponent {
                 ) : (
                     <div>
                         <Table dataSource={this.state?.dataToShowInTable} columns={this.columns} className={style.table} rowKey="orderId" />
-                        <Form labelCol={{ span: 7 }} wrapperCol={{ span: 10 }} layout="horizontal" onFinish={(values) => this.submitOrder(values)}>
+                        <Form
+                            labelCol={{ span: 7 }}
+                            wrapperCol={{ span: 10 }}
+                            layout="horizontal"
+                            onFinish={(values) => this.submitOrder(values)}
+                            ref={this.formRef}>
                             <Form.Item
-                                name="name"
-                                label="Name"
+                                name="albumName"
+                                label="Album Name"
                                 rules={[
                                     {
                                         required: true,
-                                        message: "Please input your Name!",
+                                        message: "Please input Album Name!",
                                     },
                                 ]}>
                                 <Input />
+                            </Form.Item>
+                            <Form.Item
+                                name="band"
+                                label="Artist"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input Artist Name!",
+                                    },
+                                ]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                name="category"
+                                label="Category"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please select Category",
+                                    },
+                                ]}>
+                                <Select>
+                                    {this.props.categories.map((category, index) => (
+                                        <Option key={index} value={category}>
+                                            {category}
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                name="type"
+                                label="Type"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please select Type",
+                                    },
+                                ]}>
+                                <Select>
+                                    <Option value="CD">CD</Option>
+                                    <Option value="VINYL">Vinyl</Option>
+                                    <Option value="DVD">DVD</Option>
+                                    <Option value="MP3">MP3</Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                name="price"
+                                label="Price"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please choose Price!",
+                                    },
+                                ]}>
+                                <Input type="number" />
+                            </Form.Item>
+                            <Form.Item
+                                name="amount"
+                                label="Amount"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please put available amount of products!",
+                                    },
+                                ]}>
+                                <Input type="number" />
+                            </Form.Item>
+                            <Form.Item
+                                name="imgURL"
+                                label="Image Url"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please put Image Url!",
+                                    },
+                                ]}>
+                                <Input defaultValue="assets/img/" />
                             </Form.Item>
                             <Form.Item wrapperCol={{ span: 12, offset: 12 }}>
                                 <Button type="primary" htmlType="submit">
